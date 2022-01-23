f6ee44b6aedd2e8751532880a19cf7ec018f9cb1
mindctrlSE <hello@mindctrl.se>
2022-01-23 12:39:14 +0100

    symlink script

---
+++ symlinks.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+############################
+# .make.sh
+# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
+############################
+
+########## Variables
+
+# dotfiles directory
+dir=~/Users/eric-chi/Dropbox/__igor/repos/blogs/sample-dotfiles
+
+# old dotfiles backup directory
+olddir=~/Users/eric-chi/Dropbox/__igor/repos/blogs/sample-dotfiles/dotfiles_old
+
+# list of files/folders to symlink in homedir
+files="bashrc gitconfig functions"
+
+##########
+
+# create dotfiles_old in homedir
+echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
+mkdir -p $olddir
+echo "done"
+
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
+for file in $files; do
+    echo "Moving any existing dotfiles from ~ to $olddir"
+    mv ~/.$file $olddir
+    echo "Creating symlink to $file in home directory."
+    ln -s $dir/$file ~/.$file
+done
